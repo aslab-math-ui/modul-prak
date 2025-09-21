07e3743dc10d56b55f31a62ec7bc89bf6626e831
fritzadelbertus <furitsucode@gmail.com>
2025-09-21 12:00:01 +0700

    Bug fix: modul.json

--- semuahalaman/praktikum/script.js
+++ semuahalaman/praktikum/script.js
@@ -38,3 +38,33 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch(err => console.error("Error loading JSON:", err));
 });
+
+window.document.addEventListener("DOMContentLoaded", function () {
+  if (document.getElementById('arsip-praktikum')) {
+    var options = {
+      valueNames: [ 
+        'nama-praktikum', 
+        'deskripsi', 
+        'tahun',      
+        'semester'    
+      ]
+    };
+
+    var praktikumList = new List('arsip-praktikum', options);
+
+    window.filterPraktikum = function(button) {
+      const filter = button.getAttribute('data-filter');
+      
+      praktikumList.filter(function(item) {
+        if (filter === 'all') {
+          return true;
+        } else {
+          return item.values().tahun.trim() === filter;
+        }
+      });
+
+      document.querySelectorAll('.filter-btn').forEach(btn => btn.classList.remove('active'));
+      button.classList.add('active');
+    };
+  }
+});
